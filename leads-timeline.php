7275d24695ef7cff1aecfc30268ddae0e6403f74
dzona <dzona065@gmail.com>
2011-10-29 17:38:28 +0200

    sa

---
+++ leads-timeline.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection">
+        <link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print"> 
+        <!--[if lt IE 8]>
+        <link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection">
+        <![endif]-->
+        <title></title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="span-24">
+                <div class="headerImage"></div>
+                <div id="searchwrapper">
+                    <form action="#">
+                        <input type="image" src="css/assets/15x15_search_icon.png" class="searchbox_submit" value="" />
+                        <input type="text" class="searchbox" name="s" placeholder="Search for user with email address" />
+                    </form>
+                </div>
+                <!--                    <div class="logged_user">
+                                        Philipe Sheffield <img class="header_settings" src="css/assets/16x16_header_settings.png">
+                                    </div>
+                                    <div class="user_settings_dd">
+                
+                                        <ul>
+                                            <li class="">Profile</li>
+                                            <li class="">Account</li>
+                                            <li class="">Help</li>
+                                            <li class="">Logout</li>
+                                        </ul>
+                                    </div> -->
+
+                <ul id="logged_user">
+                    <li><span>Nikola Džona Radović</span>
+                    <dd><a href="#">Profile</a></dd>
+                    <dd><a href="#">Account</a></dd>
+                    <dd><a href="#">Help</a></dd>
+                    <dd><a href="#">Logout</a></dd>
+                    </li>
+                    <div class="dd-bottom"><div class="dd-bottom-left">&nbsp;</div><div class="dd-bottom-right">&nbsp;</div></div>
+                </ul>
+            </div>
+
+            <div class="span-6">
+                <ul>
+                    <li class="dashboard active"><div class="dashboard-icon icon mrgn-icon-1"></div><span class="label">Dashboard</span></li>
+                    <li class="leads"><div class="arrow arrow mrgn-arrow"></div><div class="leads-icon icon mrgn-icon-2"></div><span class="label">Leads</span><div class="num_box"><span>1234</span></div></li>
+                    <li class="leads-email"><div class="leads-email-icon icon mrgn-icon-3"></div><span class="label">psheffie@gmail.com</span></li>
+                    <li class="leads-email"><div class="leads-email-icon icon mrgn-icon-3"></div><span class="label">soham.mehta@gmail...</span></li>
+                    <li class="leads-email"><div class="leads-email-icon icon mrgn-icon-3"></div><span class="label">dunn@magic.com</span></li>
+                    <li class="leads-email"><div class="leads-email-icon icon mrgn-icon-3"></div><span class="label">nikola.radovic@gmail...</span></li>
+                    <li class="leads-more"><div class="leads-more-icon icon mrgn-icon-3"></div><span class="label">See all recent (4)</span></li>
+                    <li class="getting_started"><div class="getting_started-icon icon mrgn-icon-1"></div><span class="label">Getting Started</span></li>
+                    <li class="tag_and_rank"><div class="tag_and_rank-icon icon mrgn-icon-1"></div><span class="label">Tag and Rank</span></li>
+                    <li class="invite_colleagues"><div class="invite_colleagues-icon icon mrgn-icon-1"></div><span class="label">Invite Colleagues</span></li>
+                </ul>
+            </div>
+            <div class="span-19">
+                <div id="page-title">www.honestdata.com</div>
+                <div class="horisontal-line">&nbsp;</div>
+                <div id="pie-holder">
+                    <div class="pie1">&nbsp;</div>
+                    <div class="pie-description">
+                        <b>Lead by score</b>
+                        <ul>
+                            <li class="lead-red">200+</li>
+                            <li class="lead-yellow">150-199</li>
+                            <li class="lead-green">100-149</li>
+                            <li class="lead-blue">50-99</li>
+                            <li class="lead-purple">0-49</li>
+                        </ul>    
+                    </div>
+                    <div class="pie2">&nbsp;</div>
+                    <div class="pie-description">
+                        <b>Popular tags</b>
+                        <ul>
+                            <li class="tag-red">Pricing</li>
+                            <li class="tag-yellow">datasheet</li>
+                            <li class="tag-green">tour</li>
+                            <li class="tag-blue">Credit Card</li>
+                            <li class="tag-purple">offer12few</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="horisontal-line">&nbsp;</div>
+                <div id="chart-holder">
+                    <div id="chart-header">
+                        <div class="chart-control">
+                            <span class="title click-icon-off bg-pos-2">Leads vs. Visitors</span><div class="icon arrow mrgn-icon-4"></div>
+
+                            <div class="input-box-wrapper">
+                                <form action="#">
+                                    <input type="text" class="inputbox" placeholder="Sep 15, 2011 - Oct 15 2011" />
+                                </form>
+                            </div>
+                            <a class="button" href="#"><span>Sort by Date<img class="mrgn-lft-5" src="css/assets/arrow_down_off.png"></span></a>
+                            <div class="dd-button-holder">
+                                <a class="button" href="#"><span>Tamo neko dugme<img class="mrgn-lft-5" src="css/assets/arrow_down_off.png"></span></a>
+                                <ul class="dd-button">
+
+                                    <li><a href="#">Pricing</a></li>
+                                    <li><a href="#" class="sort-check bg-pos-5">datasheet</a></li>
+                                    <li><a href="#">tour</a></li>
+                                    <li><a href="#">Credit Card</a></li>
+                                    <li><a href="#">offer12few</a></li>
+                                </ul>
+                                <div class="dd-bottom mrgn-bottom-2"><div class="dd-bottom-left">&nbsp;</div><div class="dd-bottom-right">&nbsp;</div></div>
+                            </div>
+                            
+                            <a class="button" href="#"><span>Sort by Date<img class="mrgn-lft-5" src="css/assets/arrow_down_off.png"></span></a>
+                        </div>
+                        <div class="horisontal-line">&nbsp;</div>
+                        <div class="chart-description">
+                            &nbsp;
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="span-24">
+                The footer
+            </div>
+        </div>
+        <div class="dd-button-holder">
+            <a class="button" href="#"><span>Tamo neko dugme<img class="mrgn-lft-5" src="css/assets/arrow_down_off.png"></span></a>
+            <ul class="dd-button">
+
+                <li><a href="#">Pricing</a></li>
+                <li class="tag-yellow"><a href="#">datasheet</a></li>
+                <li><a href="#">tour</a></li>
+                <li><a href="#">Credit Card</a></li>
+                <li><a href="#">offer12few</a></li>
+            </ul>
+            <div class="dd-bottom mrgn-bottom-2"><div class="dd-bottom-left">&nbsp;</div><div class="dd-bottom-right">&nbsp;</div></div>
+        </div>
+    </body>
+</html>
